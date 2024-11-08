520334bef01101c7295ad06f5582be9438d02718
Nisha <rainishani400@gmail.com>
2024-11-08 21:56:40 +0545

    Add files via upload

---
+++ java/javaBasic.java
@@ -0,0 +1,8 @@
+public class javaBasic{
+    public static void main(String arg[]){
+System.out.print("Hello  nisha!\n");
+System.out.println("hello world");
+System.out.println("hello");
+    }
+}
+//boilerplate codecd
