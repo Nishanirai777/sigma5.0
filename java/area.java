e7fc185436e7509b8dfbc0e980da6bce085c5213
Nisha <rainishani400@gmail.com>
2024-11-09 10:10:44 +0545

    Add files via upload

---
+++ java/area.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class area {
+    public static void main(String arg[]){
+        Scanner sc= new Scanner(System.in);
+        float rad= sc.nextFloat();
+        float area= 3.14f* rad*rad;
+        System.out.println(area);
+    }
+    
+}
