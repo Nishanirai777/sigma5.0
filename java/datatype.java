2c36144de7b01065bce78b790aa813334f3b0637
Nisha <rainishani400@gmail.com>
2024-11-09 08:30:35 +0545

    Add files via upload

---
+++ java/datatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+    public static void main(String arg[]){
+        byte b=8;
+        System.out.println(b);
+        char ch= 'n';
+        System.out.println(ch);
+        boolean var= false;
+        //float price = 3.8;
+        int number =90;
+        //byte  (range) 1
+        //short 2 bytes
+        //char  2  ""
+        //boolean 1
+        //int 4 
+        //long 8
+        //float 4
+        //double 8
+
+    }
+    
+}
