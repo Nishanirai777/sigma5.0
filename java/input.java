b549b02d1276c9f29fdacbcf79a3bb178b78bfce
Nisha <rainishani400@gmail.com>
2024-11-09 09:22:13 +0545

    Add files via upload

---
+++ java/input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        //String input = sc.next();
+        //System.out.println(input);
+        // to print the space also we use different types of input 
+        //String nisha = sc.nextLine();
+        //System.out.println(nisha);
+        //to print numbeer we use 
+       int num = sc.nextInt();
+        System.out.println(num);
+       
+
+    }
+    
+}
