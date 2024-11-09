a1348b409a564986ae2704a8172c837dd1f12911
Nisha <rainishani400@gmail.com>
2024-11-09 09:31:58 +0545

    Update sum.java

--- java/sum.java
+++ java/sum.java
@@ -8,7 +8,9 @@ public class sum {
           Scanner sc = new Scanner(System.in);
           int a = sc.nextInt();
           int b= sc.nextInt();
-          int sum = a+b;
-          System.out.println(sum);
+        //for product
+        int pro= a*b;
+          //int sum = a+b;
+         System.out.println(pro);
     }
 }
