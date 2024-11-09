05337b2ad332f94ce852e88bb1139b23757ad046
Nisha <rainishani400@gmail.com>
2024-11-09 08:03:06 +0545

    Add files via upload

---
+++ java/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String arg[]){
+    
+    int a=10;
+    int b=5;
+    System.out.println(a);
+    System.out.println(b);
+    String  name= "nisha rai";
+    System.out.println(name);
+    a=b;
+        }
+    }
+    
+
