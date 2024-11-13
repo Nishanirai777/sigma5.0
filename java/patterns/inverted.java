a6f00bff3f1e8f91fe4b9dbd2c06ae64114f0a1d
Nisha <rainishani400@gmail.com>
2024-11-13 18:02:56 +0545

    Add files via upload

---
+++ java/patterns/inverted.java
@@ -0,0 +1,12 @@
+public class inverted {
+    public static void main(String arg[]){
+        for(int line =1;line<=7; line++){
+            for(int s=1;s<=7-line+1;s++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
