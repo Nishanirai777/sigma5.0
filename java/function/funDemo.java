62cbb8e7e38b2a40ff757cfa0e523d01c9af1b52
Nisha <rainishani400@gmail.com>
2024-11-14 20:39:59 +0545

    Add files via upload

---
+++ java/function/funDemo.java
@@ -0,0 +1,10 @@
+public class funDemo {
+     public static void printhello (){
+        System.out.println("hello world");
+        System.out.println("hello nisha");
+     }
+    public static void main(String arg[])
+    {
+printhello();
+    }
+}
---
+++ java/function/product.java
@@ -0,0 +1,13 @@
+public class product {
+    public static  int multiply(int a ,int b){
+        int product =a*b;
+        return product;
+    }
+    public static void main(String arg[]){
+        int a= 34;
+        int b=9;
+        int pro=  multiply(a,b);
+        System.out.println("the product is :" + pro);
+    }
+    
+}
